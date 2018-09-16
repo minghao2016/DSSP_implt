cc30633bc32fa4e93502c5952dd396858cc1c807
kabhel <helene.kabbech@gmail.com>
2018-09-16 18:45:47 +0200

    Turns

--- dssp.py
+++ dssp.py
@@ -5,21 +5,21 @@ __author__ = "Hélène Kabbech"
 import sys
 import os
 
-# Biopython :
-from Bio.PDB import *
-
 # Implemented modules in /src foler :
 sys.path.append(os.path.abspath('src'))
 from structure import *
 from additional_functions import *
 import classes
 
+# Biopython :
+from Bio.PDB import *
+
 if __name__ == "__main__":
     opt = argsParsing()
     hydrAddition(opt.input)
     p = PDBParser(QUIET=True) # QUIET=T : Warnings issued are suppressed
     pdb = p.get_structure(opt.input,opt.input+".H")
-    #pdb = p.get_structure("1BTA","data/1bta.pdb.H")
+    #pdb = p.get_structure("1BTA","data/1itv.pdb.H")
 
     dssp = []
     index,    nb_chains = 0, 0
@@ -29,7 +29,7 @@ if __name__ == "__main__":
         first = next(res.id[1] for res in chain.get_residues() if (res.id[1] < 99999))
         # last residue id number of the current chain
         for res in chain.get_residues():
-            if (res.get_id()[0] != NONE):
+            if (res.get_id()[0] != ' '):
                 break
             last = res.get_id()[1]
 
@@ -46,4 +46,5 @@ if __name__ == "__main__":
 
     dssp = foundHelices(dssp)
     dssp = foundStrands(dssp)
+    dssp = foundNturns(dssp)
     displayResults(opt,pdb,dssp)
--- src/structure.py
+++ src/structure.py
@@ -1,8 +1,14 @@
 """@package structure
 Documentation for this module.
 
-More details.
+Secondary structure elements
+Helices
+Strands
+n-Turns
+Bridges
 """
+
+import classes
 
 ################ VARIABLES ################
 NONE = ' '
@@ -16,6 +22,7 @@ MIDDLE = { 3: '3', 4: '4', 5: '5' }
 # Secondary structural patterns :
 HELIX = { 3: 'G', 4: 'H', 5: 'I' }
 STRAND = 'E'
+TURN = 'T'
 
 # Alphabet :
 ABC_LOWER, ABC_UPPER = [], []
@@ -25,7 +32,7 @@ for letter in range(65,91):
 
 ##########################################
 
-def testHbond(ri,rj):
+def isHbond(ri,rj):
     """Test if there is an H-bond between residue i and j. Return a boolean."""
     # Fixed variables :
     Q1, Q2 = 0.41, 0.20 # partial charges
@@ -50,7 +57,7 @@ def testHbond(ri,rj):
 def isHelix(dssp,i,n):
     """n-helix(i,i+n-1) = [n-turn(i-1) and n-turn(i)]"""
     try:
-        if (testHbond(dssp[i],dssp[i+n]) == True and testHbond(dssp[i+1],dssp[i+1+n]) == True):
+        if (isHbond(dssp[i],dssp[i+n]) == True and isHbond(dssp[i+1],dssp[i+1+n]) == True):
             return(True)
         return(False)
     except:
@@ -74,7 +81,7 @@ def foundHelices(dssp):
     4-helix : >>44<<
     5-helix : >>555<<
     """
-    for res in range(len(dssp)):
+    for res in range(0,len(dssp)-2):
         # n-Helix, with n = [3,4,5]
         for n in [5,4,3]:
             if (isHelix(dssp,res,n)):
@@ -108,8 +115,8 @@ def foundHelices(dssp):
 
 def isParallelBridge(dssp,i,j):
     try:
-        if ((testHbond(dssp[i-1],dssp[j]) == True and testHbond(dssp[j],dssp[i+1]) == True)\
-        or (testHbond(dssp[j-1],dssp[i]) == True and testHbond(dssp[i],dssp[j+1]) == True)):
+        if ((isHbond(dssp[i-1],dssp[j]) == True and isHbond(dssp[j],dssp[i+1]) == True)\
+        or (isHbond(dssp[j-1],dssp[i]) == True and isHbond(dssp[i],dssp[j+1]) == True)):
             return(True)
         return(False)
     except:
@@ -117,8 +124,8 @@ def isParallelBridge(dssp,i,j):
 
 def isAntiparallelBridge(dssp,i,j):
     try:
-        if ((testHbond(dssp[i],dssp[j]) == True and testHbond(dssp[j],dssp[i]) == True)\
-        or (testHbond(dssp[i-1],dssp[j+1]) == True and testHbond(dssp[j-1],dssp[i+1]) == True)):
+        if ((isHbond(dssp[i],dssp[j]) == True and isHbond(dssp[j],dssp[i]) == True)\
+        or (isHbond(dssp[i-1],dssp[j+1]) == True and isHbond(dssp[j-1],dssp[i+1]) == True)):
             return(True)
         return(False)
     except:
@@ -126,82 +133,81 @@ def isAntiparallelBridge(dssp,i,j):
 
 def setStrandsStructure(dssp):
     for res in range(1,len(dssp)-1):
-        if ((dssp[res-1].bridge_1 != NONE or dssp[res+1].bridge_1 != NONE) \
-            and dssp[res].bridge_1!= NONE):
-            dssp[res].structure = STRAND
+        if ((dssp[res].bridge_1+dssp[res].bridge_2) != NONE+NONE):
+            if (dssp[res-1].bridge_1+dssp[res-1].bridge_2 != NONE+NONE or dssp[res+1].bridge_1+dssp[res+1].bridge_2 != NONE+NONE):
+                dssp[res].structure = STRAND
+        else:
+            if (dssp[res-1].bridge_1+dssp[res-1].bridge_2 != NONE+NONE and dssp[res+1].bridge_1+dssp[res+1].bridge_2 != NONE+NONE\
+                and (dssp[res-2].bridge_1+dssp[res-2].bridge_2 != NONE+NONE or dssp[res+2].bridge_1+dssp[res+2].bridge_2 != NONE+NONE)):
+                dssp[res].structure = STRAND
     return(dssp)
 
 def foundStrands(dssp):
     n = -1
-    s = 0
-    left = True
     for res_i in range(len(dssp)):
-        if (dssp[res_i].structure != NONE):
-            continue
-        for res_j in range(res_i+2,len(dssp)+1):
-            if (isParallelBridge(dssp,res_i,res_j) == True):
-                #dssp[res_i-1]['sheet'] = ABC_UPPER[s]
-                #dssp[res_j-1]['sheet'] = ABC_UPPER[s]
-                if (dssp[res_i-1].bridge_1 == NONE):
-                    n += 1
-                    if (n == 26): n = 0
-                if (dssp[res_i].bridge_1 == NONE):
-                    dssp[res_i].bridge_1 = ABC_LOWER[n]
-                    dssp[res_i].bp1 = res_j+1
-                else:
-                    dssp[res_i].bridge_2 = ABC_LOWER[n]
-                    dssp[res_i].bp2 = res_j+1
+        if (dssp[res_i].structure != NONE): continue
+        for res_j in range(res_i+2,len(dssp)):
+            if (dssp[res_j].structure != NONE): continue
+            if (isParallelBridge(dssp,res_i,res_j) == True or isAntiparallelBridge(dssp,res_i,res_j) == True):
+                if (isParallelBridge(dssp,res_i,res_j) == True):
+                    alphabet = ABC_LOWER
+                elif (isAntiparallelBridge(dssp,res_i,res_j) == True):
+                    alphabet = ABC_UPPER
 
-                if (dssp[res_j].bridge_1 == NONE):
-                    dssp[res_j].bridge_1 = ABC_LOWER[n]
-                    dssp[res_j].bp1 = res_i+1
-                else:
-                    dssp[res_j].bridge_2 = ABC_LOWER[n]
-                    dssp[res_j].bp2 = res_i+1
-                if (dssp[res_i].bridge_2 != NONE):
-                    s += 1
-    setStrandsStructure(dssp)                    
-    return(dssp)
-"""
-def foundStrands(dssp,chain):
-    n = -1
-    s = 0
-    for res_i in range(len(dssp)):
-        if (dssp[res_i]['structure'] != STRUC['none']):
-            continue
-        for res_j in range(res_i+2,len(dssp)+1):
-                if (isParallelBridge(chain,res_i,res_j) == True):
-                    #dssp[res_i-1]['sheet'] = ABC_UPPER[s]
-                    #dssp[res_j-1]['sheet'] = ABC_UPPER[s]
-                    if (dssp[res_i-2]['bridge 1'] == NONE):
+                if (dssp[res_i+1].bp1 == 0 and dssp[res_i-1].bp2 == 0):
+                    if (dssp[res_i-1].bp1 == 0):
                         n += 1
                         if (n == 26): n = 0
-                    if (dssp[res_i-1]['bridge 1'] == NONE):
-                        dssp[res_i-1]['bridge 1'] = ABC_LOWER[n]
-                        dssp[res_i-1]['bp1'] = res_j
-                    else:
-                        dssp[res_i-1]['bridge 2'] = ABC_LOWER[n]
-                        dssp[res_i-1]['bp2'] = res_j
-
-                    if (dssp[res_j-1]['bridge 1'] == NONE):
-                        dssp[res_j-1]['bridge 1'] = ABC_LOWER[n]
-                        dssp[res_j-1]['bp1'] = res_i
-                    else:
-                        dssp[res_j-1]['bridge 2'] = ABC_LOWER[n]
-                        dssp[res_j-1]['bp2'] = res_i
-                    if (dssp[res_i-1]['bridge 2'] != NONE):
-                        s += 1
+                    dssp[res_i].bp1 = res_j+1
+                    dssp[res_i].bridge_1 = alphabet[n]
+                else:
+                    dssp[res_i].bp2 = res_j+1
+                    if (dssp[res_i-1].bp2 == 0 and dssp[res_i-2].bp2 == 0): # New strand
+                        n += 1
+                        if (n == 26): n = 0
+                    dssp[res_i].bridge_2 = alphabet[n]
+                dssp[res_j].bp1 = res_i+1
+                dssp[res_j].bridge_1 = alphabet[n]
     setStrandsStructure(dssp)                    
     return(dssp)
-"""
 
-"""
-                elif (isAntiparallelBridge(chain,res_i,res_j) == True):
-                    if (dssp[res_i-2]['bridge']['api'] == ''):
-                        N += 1
-                        if (N == 26): N = 0
-                    dssp[res_i-1]['bridge']['api'] += ABC_UPPER[N]
-                    dssp[res_i-1]['bp2'] = res_j
-                    dssp[res_j-1]['bridge']['apj'] += ABC_UPPER[N]
-                    dssp[res_j-1]['bp1'] = res_i 
-"""
+def setNturnsStructure(dssp):
+    """ """
+    for n in [5,4,3]:
+        for res in range(len(dssp)):
+            if (dssp[res].structure != NONE): continue
+            if (dssp[res].nturns[n].result == START):
+                while (dssp[res+2].nturns[n].result != NONE):
+                    res += 1
+                    if (dssp[res].structure == NONE):
+                        dssp[res].structure = TURN
+    return(dssp)
+
+
+
+def foundNturns(dssp):
+    for res in range(0,len(dssp)-3):
+        if (dssp[res].structure != NONE): continue
+        for n in [5,4,3]:
+            if (isHbond(dssp[res],dssp[res+n]) == True):
+                dssp[res].nturns[n].start = START
+                dssp[res+n].nturns[n].end =  END
+                for i in range(1,n):
+                    dssp[res+i].nturns[n].middle =  MIDDLE[n]
+            if (dssp[res].nturns[n].end == END):
+                if (dssp[res].nturns[n].start == START):
+                    # '>' + '[3,4,5]' + '<' = 'X' (start & end)
+                    dssp[res].nturns[n].result = START_END
+                else:
+                    # ' ' + '<' = '<' (end)
+                    dssp[res].nturns[n].result = END
+            else:
+                if (dssp[res].nturns[n].start == START):
+                    # '>' + ' ' = '>' (start)
+                    dssp[res].nturns[n].result = START
+                else:
+                    if (dssp[res].nturns[n].middle != NONE):
+                        # (' ' + ' ') and (middle != ' ') =  [3,4,5] (middle)
+                        dssp[res].nturns[n].result = MIDDLE[n]
+    setNturnsStructure(dssp)
+    return(dssp)
